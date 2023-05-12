21c8f1cd3c223a5b4281cd9d5a41c846a0a49cee
Jisu Lee <dlwltn0430@naver.com>
2023-05-12 15:44:37 +0900

    style: navbar 아래 이미지 영역 컴포넌트화

---
+++ src/components/sub_top/sub_top.jsx
@@ -0,0 +1,14 @@
+import './sub_top.css';
+
+export default function SubTop() {
+    return(
+        <div className='sub_top'>
+            <div className='top_img'>
+                <div>
+                    <div className='content' style={{fontWeight: 'bold'}}>ABOUT</div>
+                    <div className='content' style={{fontSize: '3rem'}}>학회소개</div>
+                </div>
+            </div>
+        </div>
+    )
+}

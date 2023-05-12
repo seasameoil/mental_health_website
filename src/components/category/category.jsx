bb4fbef83e5297ea24ec40e8c3a8c1af5d48c677
Jisu Lee <dlwltn0430@naver.com>
2023-05-12 15:59:31 +0900

    fix: Link 경로 수정

--- src/components/category/category.jsx
+++ src/components/category/category.jsx
@@ -12,7 +12,7 @@ export default function Category() {
                         <Link to={"/introduce/sub01/1"}>
                             <li className='sub-category'>학회소개</li>
                         </Link>
-                        <Link to={"introduce/sub01/2"}>
+                        <Link to={"/introduce/sub01/2"}>
                             <li className='sub-category'>임상심리학소개</li>
                         </Link>
                     </div>
@@ -23,14 +23,14 @@ export default function Category() {
                 </ul>
 
                 <ul>
-                    <Link to={"introduce/sub03/1"}>
+                    <Link to={"/introduce/sub03/1"}>
                         <div>학회발자취</div>
                     </Link>
                     <div className='sub-category-box'>
-                        <Link to={"introduce/sub03/1"}>
+                        <Link to={"/introduce/sub03/1"}>
                             <li className='sub-category'>연혁</li>
                         </Link>
-                        <Link to={"introduce/sub03/2"}>
+                        <Link to={"/introduce/sub03/2"}>
                             <li className='sub-category'>역대회장</li>
                         </Link>
                     </div>
@@ -39,16 +39,16 @@ export default function Category() {
                 <ul>
                     <div>학회 규정</div>
                     <div className='sub-category-box'>
-                        <Link to={"introduce/sub04/1"}>
+                        <Link to={"/introduce/sub04/1"}>
                             <li className='sub-category'>회칙 및 운영세칙</li>
                         </Link>
-                        <Link to={"introduce/sub04/2"}>
+                        <Link to={"/introduce/sub04/2"}>
                             <li className='sub-category'>윤리규정</li>
                         </Link>
-                        <Link to={"introduce/sub04/3"}>
+                        <Link to={"/introduce/sub04/3"}>
                             <li className='sub-category'>임상심리전문가 관련 규정</li>
                         </Link>
-                        <Link to={"introduce/sub04/4"}>
+                        <Link to={"/introduce/sub04/4"}>
                             <li className='sub-category'>인권 및 회원권익위원회 운영규정</li>
                         </Link>
                     </div>
@@ -57,25 +57,25 @@ export default function Category() {
                 <ul>
                     <div>조직</div>
                     <div className='sub-category-box'>
-                        <Link to={"introduce/sub05/1"}>
+                        <Link to={"/introduce/sub05/1"}>
                             <li className='sub-category'>임원진</li>
                         </Link>
-                        <Link to={"introduce/sub05/2"}>
+                        <Link to={"/introduce/sub05/2"}>
                             <li className='sub-category'>감사</li>
                         </Link>
-                        <Link to={"introduce/sub05/3"}>
+                        <Link to={"/introduce/sub05/3"}>
                             <li className='sub-category'>TF/(임시)위원회</li>
                         </Link>
-                        <Link to={"introduce/sub05/4"}>
+                        <Link to={"/introduce/sub05/4"}>
                             <li className='sub-category'>대의원회</li>
                         </Link>
-                        <Link to={"introduce/sub05/5"}>
+                        <Link to={"/introduce/sub05/5"}>
                             <li className='sub-category'>협의회</li>
                         </Link>
-                        <Link to={"introduce/sub05/6"}>
+                        <Link to={"/introduce/sub05/6"}>
                             <li className='sub-category'>지회</li>
                         </Link>
-                        <Link to={"introduce/sub05/7"}>
+                        <Link to={"/introduce/sub05/7"}>
                             <li className='sub-category'>연구회</li>
                         </Link>
                     </div>

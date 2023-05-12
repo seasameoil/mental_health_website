bff720b81c1ceee5457a93b5d4b2d11641843a9a
Jisu Lee <dlwltn0430@naver.com>
2023-05-13 01:26:19 +0900

    feat: route path 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React from "react";
 import { Route, Router, Routes } from "react-router-dom";
 import Nav from "./components/Nav";
-import Introduce from "./Pages/Introduce";
+import Footer from "./components/footer/footer";
+import Introduce from "./Pages/introduce/Sub01_1_intro";
 import News from "./Pages/News";
 import Information from "./Pages/Information";
 import Education from "./Pages/Education";
 import Journal from "./Pages/Journal";
+//(1) 학회 소개
+import Sub01_1_intro from "./Pages/introduce/Sub01_1_intro";
+import Sub01_2_intro from "./Pages/introduce/Sub01_2_intro";
+import Sub02_intro from "./Pages/introduce/Sub02_intro";
+import Sub03_1_intro from "./Pages/introduce/Sub03_1_intro";
+import Sub03_2_intro from "./Pages/introduce/Sub03_2_intro";
+import Sub04_1_intro from "./Pages/introduce/Sub04_1_intro";
+import Sub04_2_intro from "./Pages/introduce/Sub04_2_intro";
+import Sub04_3_intro from "./Pages/introduce/Sub04_3_intro";
+import Sub04_4_intro from "./Pages/introduce/Sub04_4_intro";
+import Sub05_1_intro from "./Pages/introduce/Sub05_1_intro";
+import Sub05_2_intro from "./Pages/introduce/Sub05_2_intro";
+import Sub05_3_intro from "./Pages/introduce/Sub05_3_intro";
+import Sub05_4_intro from "./Pages/introduce/Sub05_4_intro";
+import Sub05_5_intro from "./Pages/introduce/Sub05_5_intro";
+import Sub05_6_intro from "./Pages/introduce/Sub05_6_intro";
+import Sub05_7_intro from "./Pages/introduce/Sub05_7_intro";
+
 import Sub01 from "./Pages/spec_news/Sub01";
 import Sub02 from "./Pages/spec_news/Sub02";
 import Sub03 from "./Pages/spec_news/Sub03";
@@ -15,7 +34,7 @@ function App() {
   return (
     <div className="App">
       <Nav />
-      
+
       <Routes>
         <Route path="/introduce" element={<Introduce />} />
         <Route path="/news" element={<News />} />
@@ -25,12 +44,33 @@ function App() {
       </Routes>
 
       <Routes>
+        <Route path="/introduce/sub01/1" element={<Sub01_1_intro />} />
+        <Route path="/introduce/sub01/2" element={<Sub01_2_intro />} />
+        <Route path="/introduce/sub02" element={<Sub02_intro />} />
+        <Route path="/introduce/sub03/1" element={<Sub03_1_intro />} />
+        <Route path="/introduce/sub03/2" element={<Sub03_2_intro />} />
+        <Route path="/introduce/sub04/1" element={<Sub04_1_intro />} />
+        <Route path="/introduce/sub04/2" element={<Sub04_2_intro />} />
+        <Route path="/introduce/sub04/3" element={<Sub04_3_intro />} />
+        <Route path="/introduce/sub04/4" element={<Sub04_4_intro />} />
+        <Route path="/introduce/sub05/1" element={<Sub05_1_intro />} />
+        <Route path="/introduce/sub05/2" element={<Sub05_2_intro />} />
+        <Route path="/introduce/sub05/3" element={<Sub05_3_intro />} />
+        <Route path="/introduce/sub05/4" element={<Sub05_4_intro />} />
+        <Route path="/introduce/sub05/5" element={<Sub05_5_intro />} />
+        <Route path="/introduce/sub05/6" element={<Sub05_6_intro />} />
+        <Route path="/introduce/sub05/7" element={<Sub05_7_intro />} />
+      </Routes>
+
+      {/*
+      <Routes>
         <Route path="/news/sub01" element={<Sub01 />} />
-        <Route path="/news/sub02" element={<Sub02 />} />
+        <Route path="/news/sub02" element={<Sub02_ />} />
         <Route path="/news/sub03" element={<Sub03 />} />
         <Route path="/news/sub04" element={<Sub04 />} />
       </Routes>
-      
+  */}
+      <Footer/>
     </div>
   );
 }

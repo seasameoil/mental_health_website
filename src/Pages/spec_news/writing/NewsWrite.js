ce1f3f634f8e0ed1e2f2c86ed392dbeaff93d381
Jisu Lee <dlwltn0430@naver.com>
2023-06-25 22:08:46 +0900

    fix: 글쓰기 기능 미완

--- src/Pages/spec_news/writing/NewsWrite.js
+++ src/Pages/spec_news/writing/NewsWrite.js
@@ -1,30 +1,34 @@
 import { useState } from "react";
 import { addDoc, collection, getFirestore } from "firebase/firestore";
+import { upload } from "@testing-library/user-event/dist/upload";
 export const dbService = getFirestore();
 
 export default function NewsWrite() {
-    const [title, setTitle] = useState('');
-    const [content, setContent] = useState('');
-    const handleTitle = (event) => {
-        const {
-          target: { value },
-        } = event;
-        setTitle(value);
-    };
-    const handleContents = (event) => {
-        const {
-          target: { value },
-        } = event;
-        setContent(value);
-    };
+    const [form, setForm] = useState({
+        title: "",
+        content: "",
+        type: ""
+      });
+
+    function handleChange(event) {
+        const { name, value } = event.target;
+        setForm(prevState => ({
+          ...prevState,
+          [name]: value
+        }));
+      }
+      //console.log(form)
+
     const handleSubmit = async (e) => {
+        e.preventDefault();
         try {
             const docRef = await addDoc(collection(dbService, "notification"), {
-                title: title,
-                content: content,
-                /*createAt: Date.now(),*/
+                title: form.title,       
+                content: form.content,  
+                type: form.type,       
+                uploadTime: Date.now(),
             });
-            setContent(docRef);
+            console.log(docRef)
         } catch (error) {
             console.log(error);
         }
@@ -34,11 +38,15 @@ export default function NewsWrite() {
         <div>
             <div>
                 제목
-                <input value={title} onChange={handleTitle}></input>
+                <input name="title" value={form.title} onChange={handleChange}></input>
             </div>
             <div>
                 내용
-                <input value={content} onChange={handleContents}></input>
+                <input name="content" value={form.content} onChange={handleChange}></input>
+            </div>
+            <div>
+                분류
+                <input name="type" value={form.type} onChange={handleChange}></input>
             </div>
             <div>
                 <button onClick={handleSubmit}>등록</button>
